454b0a9e2c495de27d523c8359014e36c5280bb8
yarum-git <125512131+yarum-git@users.noreply.github.com>
2023-02-16 15:07:38 +0400

    Delete Install.sh

--- Install.sh
+++
@@ -1,1 +0,0 @@
-sudo dnf install gnome-tweaks @development-tools "Hardware Support" gnome-terminal-nautilus xdg-user-dirs xdg-user-dirs-gtk ffmpegthumbnailer gnome-calculator gnome-system-monitor evince file-roller
