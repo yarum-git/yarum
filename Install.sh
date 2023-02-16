df9b03366b183b9470ba85a97c959d73c9ea1e0d
yarum-git <125512131+yarum-git@users.noreply.github.com>
2023-02-16 15:02:32 +0400

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,1 @@
+sudo dnf install gnome-tweaks @development-tools "Hardware Support" gnome-terminal-nautilus xdg-user-dirs xdg-user-dirs-gtk ffmpegthumbnailer gnome-calculator gnome-system-monitor evince file-roller
